75800d8cfd8c5f95e688658081bb35901af560dd
Fujiwara Taku <dev.taklatz@gmail.com>
2022-12-16 02:08:10 +0900

    edit server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const fs = require('fs');
 
-const HOST_NAME = '127.0.0.1';
+const HOST_NAME = '0.0.0.0';
 const PORT = 3000;
 
 const server = http.createServer((req, res) => {
